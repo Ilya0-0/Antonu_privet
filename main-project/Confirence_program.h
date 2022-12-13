7bad18c43543fb6c67260b5e95ed1a7059bddc84
Илья <ilya-butov-00@mail.ru>
2022-12-13 23:12:46 +0300

    Добавлены структуры для хранения данных

--- main-project/Confirence_program.h
+++ main-project/Confirence_program.h
@@ -1,1 +1,28 @@
 #pragma once
+#ifndef Confirence_program_H
+#define Confirence_program_H
+
+#include "constants.h"
+
+struct date
+{
+    int hour;
+    int minutes;
+};
+
+struct person
+{
+    char first_name[MAX_STRING_SIZE];
+    char middle_name[MAX_STRING_SIZE];
+    char last_name[MAX_STRING_SIZE];
+};
+
+struct Confirence_program
+{
+    person lecturer;
+    date start;
+    date finish;
+    char title[MAX_STRING_SIZE];
+};
+
+#endif
