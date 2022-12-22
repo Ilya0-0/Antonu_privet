ea1747e59b60a3d048a1d00d4ee60f5eaac7c741
Илья <ilya-butov-00@mail.ru>
2022-12-22 18:23:31 +0300

    Создана функция-заглушка для обработки данных

---
+++ main-project/process.cpp
@@ -0,0 +1,6 @@
+#include "processing.h"
+
+int process(Confirence_program* array[], int size)
+{
+	return 0;
+}
---
+++ main-project/processing.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef PROCESSING_H
+#define PROCESSING_H
+
+#include "Confirence_program.h"
+
+int process(Confirence_program* array[], int size);
+
+#endif
+
